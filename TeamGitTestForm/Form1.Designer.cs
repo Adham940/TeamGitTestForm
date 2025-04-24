c6fe5983d3d83fe5fa1719fac15104f9020ac3bb
Ahmed <ahmedmahmoud1126u@gmail.com>
2025-04-24 22:34:38 +0200

    Add lblAHMED

--- TeamGitTestForm/Form1.Designer.cs
+++ TeamGitTestForm/Form1.Designer.cs
@@ -28,12 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            lblAHMED = new Label();
+            SuspendLayout();
+            // 
+            // lblAHMED
+            // 
+            lblAHMED.AutoSize = true;
+            lblAHMED.Location = new Point(461, 258);
+            lblAHMED.Name = "lblAHMED";
+            lblAHMED.Size = new Size(114, 41);
+            lblAHMED.TabIndex = 0;
+            lblAHMED.Text = "Ahmed";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1099, 695);
+            Controls.Add(lblAHMED);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblAHMED;
     }
 }
