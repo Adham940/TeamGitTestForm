9d15d23b63b74bc449f557174b8665bc6624c71b
S7S <hussainelasftawy1380@gmail.com>
2025-04-24 23:05:34 +0200

    Move btn

--- TeamGitTestForm/S7S.Designer.cs
+++ TeamGitTestForm/S7S.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            button1.Location = new Point(170, 12);
+            button1.Location = new Point(182, 108);
             button1.Name = "button1";
             button1.Size = new Size(206, 29);
             button1.TabIndex = 0;
